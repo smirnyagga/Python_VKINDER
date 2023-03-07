0849709da5dbef2b6d9509b6a2fa52cf7f7ed733
Елена Смирнова <nomsgva@gmail.com>
2023-03-07 18:23:41 +0300

    changed

--- main.py
+++ main.py
@@ -71,10 +71,10 @@ def get_user_info(user_id):
                 user_info['age'] = get_age(value)
             else:
                 user_info[key] = value
+        return user_info
     else:
         write_msg(user_id, 'Ошибка')
         return False
-    return user_info
 
 
 def check_info(user_info):
@@ -100,43 +100,56 @@ def add_bdate(user_id):
                     write_msg(user_id, 'Дата указана неверно, введите ДАТУ РОЖДЕНИЯ в формате ДД.ММ.ГГГГ')
 
 
-
 def add_city(user_id):
     a = False
-    while a == False:
+    while a is False:
         write_msg(user_id, 'Недостаточно информации, введите город в Именительном падеже')
         for event in VkLongPoll(session).listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                response = session_vk.method('database.getCities', {
-                    'country_id': 1,
-                    'q': event.text,
-                    'need_all': 0,
-                    'count': 1
-                })
-                if response['count'] == 0:
-                    write_msg(user_id, 'Город указан неверно, введите город в именительном падеже')
-                else:
-                    user_info['city'] = response['items'][0]['id']
+                try:
+                    response = session_vk.method('database.getCities', {
+                        'country_id': 1,
+                        'q': event.text,
+                        'need_all': 0,
+                        'count': 1
+                    })
+
+                    if response['count'] == 0:
+                        write_msg(user_id, 'Город указан неверно, введите город в именительном падеже')
+                    else:
+                        user_info['city'] = response['items'][0]['id']
+                        a = True
+                        return user_info
+
+                except Exception as e:
+                    # write_msg(user_id, 'Ошибка, попробуйте позже')
+                    print(e)
                     a = True
-                    return user_info
+                    return None
 
 
-# получаем ай ди всех пользователей, подходящих под запросы
 def serch_users(user_info):
-    response = session_vk.method('users.search', {
-        'age_from': user_info['age']-3,
-        'age_to': user_info['age']+3,
-        'sort': 0,
-        'count': 1000,
-        'city': user_info['city'],
-        'sex': 3 - user_info['sex'],
-        'status': 6,
-        'has_photo': 1})
+    try:
+        response = session_vk.method('users.search', {
+        # response = session_vk.method('users.search', {
+            'age_from': user_info['age']-3,
+            'age_to': user_info['age']+3,
+            'sort': 0,
+            'count': 100,
+            'city': user_info['city'],
+            'sex': 3 - user_info['sex'],
+            'status': 6,
+            'has_photo': 1})
+
+    except Exception as e:
+        print(e)
+        return None
 
     list_users_id = []
-    for item in response['items']:
-        if item['is_closed'] == False:
-            list_users_id.append(item['id'])
+    if len(response['items']) != 0:
+        for item in response['items']:
+            if item['is_closed'] is False:
+                list_users_id.append(item['id'])
     return list_users_id
 
 
@@ -156,27 +169,37 @@ def check_id(list_users_id):
     return new_list_users_id       # уже обновленный возвращает, без старых ай ди
 
 
-# получаем словарь с информацией о 3 самых популярных фото для каждого подходящего пользователя
-# а потом список, где будет строка с фото для вложений и ID претендента
-def get_fotos_info(list_users_id):
-    info_fotos_for_upload = {}
-    response = session_vk.method('photos.get', {
-        'owner_id': list_users_id[0],
-        'album_id': 'profile',
-        'extended': 1})
-
-    popular_list = []        # сюда будем заносить инфу о 3 самых популярных фото
-    likes_list = []        # здесь создадим список из лайков, отранжируем по убыванию, чтобы потом выбрать самые популярные фото
+# функция для поиска из списка 3 самых популярных фото
+def serch_popular(response):
+    popular_list = []  # сюда будем заносить инфу о 3 самых популярных фото
+    likes_list = []  # создадим список из лайков, ранжируем по убыванию, чтобы потом выбрать самые популярные фото
     for foto in response['items']:
         likes_list.append(foto['likes']['count'] + foto['comments']['count'])
-    nessesary_likes = sorted(likes_list, reverse=True)     # отсортировали по убыванию и будем брать только первые 3 значения
+    nessesary_likes = sorted(likes_list, reverse=True)  # отсортировали по убыванию, брать только первые 3 значения
 
-    while len(popular_list) < 3:  # пока не отберем 3 самые популярные фотки, сравниваем каждое фото с максимальными лайками
+    while len(popular_list) < 3:  # пока не отберем 3 популярные фотo, сравниваем каждое фото с максимальными лайками
         for foto in response['items']:
-            if foto['likes']['count'] + foto['comments']['count'] == nessesary_likes[0] or foto['likes']['count'] + foto['comments']['count'] == nessesary_likes[1] or foto['likes']['count'] + foto['comments']['count'] == nessesary_likes[2]:
+            if foto['likes']['count'] + foto['comments']['count'] == nessesary_likes[0] or foto['likes']['count'] + \
+                    foto['comments']['count'] == nessesary_likes[1] or foto['likes']['count'] + foto['comments']['count'] == nessesary_likes[2]:
                 popular_list.append(foto)
+    return popular_list
 
-    info_ids_fotos = []    # в следующем цикле от каждой популярной фотки берем ай ди фото и пользователя для дальнейшей загрузки
+
+# функция для поиска фото и формирования списка вложений
+def get_fotos_info(list_users_id):
+    info_fotos_for_upload = {}
+    try:
+        response = session_vk.method('photos.get', {
+            'owner_id': list_users_id[0],
+            'album_id': 'profile',
+            'extended': 1})
+    except Exception as e:
+        print(e)
+        return None
+
+    popular_list = serch_popular(response)
+
+    info_ids_fotos = []  # в следующем цикле от каждой популярной фото берем id фото и пользователя
     for item in popular_list:
         info_ids_fotos.append(item['id'])
     info_fotos_for_upload[popular_list[0]['owner_id']] = info_ids_fotos
@@ -185,7 +208,7 @@ def get_fotos_info(list_users_id):
 
     person = list_users_id[0]
 
-    list_foto_attachments = []  # список вложений каждого отдельного пользователя формата ['photo7097751_266247043', 'photo7097751_331530314', 'photo7097751_457252016']
+    list_foto_attachments = []  # список вложений каждого отдельного пользователя формата ['photo7097751_266247043',..]
     for couple in info_fotos_for_upload[person]:
         list_foto_attachments.append(f'photo{person}_{couple}')
 
@@ -199,22 +222,20 @@ def get_fotos_info(list_users_id):
 
 
 # функция для отправки текста сообщения
-def write_msg(user_id, message):
-    session.method('messages.send', {
-        'user_id': user_id,
-        'message': message,
-        'random_id': randrange(10 ** 7)
-    })
-
-
-# функция для отправки текста сообщения с разными клавиатурами
-def write_msg_key(user_id, message, key):
-    session.method('messages.send', {
-        'user_id': user_id,
-        'message': message,
-        'random_id': randrange(10 ** 7),
-        'keyboard': key.get_keyboard()
-    })
+def write_msg(user_id, message, key=None):
+    if key is not None:
+        session.method('messages.send', {
+            'user_id': user_id,
+            'message': message,
+            'random_id': randrange(10 ** 7),
+            'keyboard': key.get_keyboard()
+        })
+    else:
+        session.method('messages.send', {
+            'user_id': user_id,
+            'message': message,
+            'random_id': randrange(10 ** 7)
+        })
 
 
 # функция для отправки фотографий
@@ -232,7 +253,7 @@ for event in VkLongPoll(session).listen():
         user_id = event.user_id
 
         if txt == "привет":
-            write_msg_key(user_id, f"Хай, {user_id}. Напиши 'старт', если хочешь подобрать себе пару", keyboard_start)
+            write_msg(user_id, f"Хай, {user_id}. Напиши 'старт', если хочешь подобрать себе пару", key=keyboard_start)
             user_info = get_user_info(user_id)
             missing_list = check_info(user_info)
 
@@ -240,29 +261,51 @@ for event in VkLongPoll(session).listen():
             if 'age' in missing_list:
                 add_bdate(user_id)
             if 'city' in missing_list:
-                add_city(user_id)
+                city = add_city(user_id)
+            if city is not None:
+                list_users_id = serch_users(user_info)
 
-            list_users_id = serch_users(user_info)
-            write_msg_key(user_id, "Я подобрала для тебя несколько вариантов. Ответь 'дальше', если хочешь продолжить", keyboard_forward)
+                if list_users_id is not None:
+                    if list_users_id == []:
+                        write_msg(user_id, 'В вашем городе по вашим запросам ни не найдено')
 
-            create_tables(engine)
-            new_list_users_id = check_id(list_users_id)
+                    else:
+                        new_list_users_id = check_id(list_users_id)
+                        if new_list_users_id == []:
+                            write_msg(user_id, 'Вы уже просмотрели все варианты')
+                        else:
+                            write_msg(user_id,
+                                      "Я подобрала несколько вариантов. Ответь 'дальше', если хочешь продолжить",
+                                      key=keyboard_forward)
+
+                            create_tables(engine)
+                            new_list_users_id = check_id(list_users_id)
+
+                else:
+                    write_msg(user_id, "Ошибка, попробуйте позже")
+            else:
+                write_msg(user_id, "Ошибка, попробуйте позже")
 
         elif txt == "дальше":
-            write_msg_key(user_id, 'отправляю фотографии. Ответь "дальше", если хочешь продолжить. Ответь "пока", если хочешь завершить', keyboard_main)
-            new_list_users_id = check_id(list_users_id)
+            write_msg(user_id,
+                      'отправляю фотографии. Ответь "дальше", если хочешь продолжить. Ответь "пока", если хочешь завершить',
+                      key=keyboard_main)
+
             list_attach = get_fotos_info(new_list_users_id)
+            if list_attach is not None:
 
-            send_foto(user_id, list_attach)
+                send_foto(user_id, list_attach)
 
-            option1 = Options(user_id=user_id, option_id=list_attach[1], foto=list_attach[0], white_list='not defined')
-            sessiondb.add_all([option1])
-            sessiondb.commit()
+                option1 = Options(user_id=user_id, option_id=list_attach[1], foto=list_attach[0], white_list='not defined')
+                sessiondb.add_all([option1])
+                sessiondb.commit()
+            else:
+                write_msg(user_id, "Ошибка в поиске фотографий, попробуйте позже")
 
         elif txt == "добавить в избранное":
             sessiondb.query(Options).filter(Options.option_id == list_attach[1] and Options.user_id == user_id).update({"white_list": "favourite"})
             sessiondb.commit()
-            write_msg_key(user_id, "Добавлено в избранное. Ответь 'дальше', если хочешь продолжить", keyboard_forward)
+            write_msg(user_id, "Добавлено в избранное. Ответь 'дальше', если хочешь продолжить", key=keyboard_forward)
 
         elif txt == "мое избранное":
             q = sessiondb.query(Options).filter(Options.white_list == 'favourite' and Options.user_id == user_id)
@@ -270,7 +313,7 @@ for event in VkLongPoll(session).listen():
             for s in q.all():
                 message = message + f"https://vk.com/id{s.option_id}" + '\n'
             write_msg(user_id, message)
-            write_msg_key(user_id, "Продолжаем? :)", keyboard_forward)
+            write_msg(user_id, "Продолжаем? :)", key=keyboard_forward)
 
         elif txt == "пока":
             write_msg(user_id, "Удачи!")
